03e24c513e61a20f2d82d08dfc97e021d9fbad00
walizada <walizada.official@gmail.com>
2026-03-10 21:44:22 +0100

    fix vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/drivepro/",
+  base: mode === "production" ? "/drivepro/" : "/",
 
   server: {
     host: "::",
