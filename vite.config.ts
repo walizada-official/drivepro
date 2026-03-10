112b541865843ebde00bf67dec9c6606a9a53893
walizada <walizada.official@gmail.com>
2026-03-10 20:04:28 +0100

    deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.VITE_BASE_PATH || "/",
+  base: "/drivepro/",
+
   server: {
     host: "::",
     port: 8080,
@@ -13,7 +13,9 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
